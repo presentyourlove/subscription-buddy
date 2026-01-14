4098409a8174cd73c1396483eb57f6b552b0599f
jasonwth <jasonwth0928@gmail.com>
2026-01-14 20:31:25 +0800

    fix(lint): 修復 ESLint 設定以支援 TypeScript 和 Service Worker
    - 新增 .eslintignore 忽略 Vue 中間檔案
    - 更新 .eslintrc.cjs 支援 TypeScript 解析
    - 為 firebase-messaging-sw.js 設定 Service Worker 全域變數
    - 安裝 @typescript-eslint/parser 和 @typescript-eslint/eslint-plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,36 @@
 /* eslint-env node */
 module.exports = {
   root: true,
-  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    ecmaVersion: 'latest'
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
+  ignorePatterns: ['*.vue.js', '*.vue.js.map', 'dist/**', 'node_modules/**'],
   rules: {
     'vue/multi-word-component-names': 'off',
-    'no-unused-vars': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
     'prettier/prettier': 'error'
-  }
+  },
+  overrides: [
+    {
+      files: ['public/firebase-messaging-sw.js'],
+      env: {
+        serviceworker: true
+      },
+      globals: {
+        firebase: 'readonly',
+        importScripts: 'readonly'
+      }
+    }
+  ]
 }
