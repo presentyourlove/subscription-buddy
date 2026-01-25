47e840f66f5886bea6f38a975daa6edd82a7a6d0
jasonwth <jasonwth0928@gmail.com>
2026-01-25 14:58:58 +0800

    fix(core): remove invalid firebase config import in NotificationService
    - Use getMessaging() instead of importing missing config file

--- packages/core/src/utils/notificationService.ts
+++ packages/core/src/utils/notificationService.ts
@@ -1,16 +1,19 @@
-import { getToken } from 'firebase/messaging'
+import { getToken, getMessaging } from 'firebase/messaging'
 
-import { messaging } from '../firebase/config'
+// import { messaging } from '../firebase/config'
 
 export const requestNotificationPermission = async (): Promise<string | undefined> => {
   try {
     const permission = await Notification.requestPermission()
-    if (permission === 'granted' && messaging) {
-      const token = await getToken(messaging, {
-        vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY // User needs to add this to .env
-      })
-      // TODO: Save token to Firestore users/{uid}/fcmTokens
-      return token
+    if (permission === 'granted') {
+      const messaging = getMessaging()
+      if (messaging) {
+        const token = await getToken(messaging, {
+          vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY // User needs to add this to .env
+        })
+        // TODO: Save token to Firestore users/{uid}/fcmTokens
+        return token
+      }
     } else {
       // Permission denied
     }
